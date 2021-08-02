3c64d681f63273f8213983d1218d5640bfd78bd8
fede <federico.caiello1994@gmail.com>
2021-08-02 13:01:26 +0200

    mobile update

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,17 +1,19 @@
-<template class="absolute inset-x-0 top-0">
-  <div class="px-5 lg:px-40 container mx-auto lg:flex lg:items-center justify-between lg:py-8 space-y-8 lg:space-y-0">
-    <div class=" lg:w-4/12">
-      <ul class="lg:flex lg:justify-start text-2xl text-blue-600 font-sans space-y-4 lg:space-y-0 lg:space-x-12">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </div>
-    <div class="lg:w-4/12 lg:flex lg:justify-center">
-      <g-image src="~/assets/Landie.png" width=""/>
-    </div>
-    <div class="lg:w-4/12 lg:flex lg:justify-end">
-        <button class="text-white bg-blue-700 px-10 py-1">Buy Now</button>
+<template>
+  <div class="absolute inset-x-0 top-0">
+    <div class="px-5 lg:px-40 container mx-auto lg:flex lg:items-center justify-between lg:py-8 space-y-8 lg:space-y-0">
+      <div class="lg:w-4/12">
+        <ul class="lg:flex lg:justify-start text-2xl text-blue-600 font-sans space-y-4 lg:space-y-0 lg:space-x-12">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div class="lg:w-4/12 lg:flex lg:justify-center">
+        <g-image src="~/assets/Landie.png" width=""/>
+      </div>
+      <div class="lg:w-4/12 lg:flex lg:justify-end">
+          <button class="text-white bg-blue-700 px-10 py-1">Buy Now</button>
+      </div>
     </div>
   </div>
 </template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,26 @@
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-
-    <!-- <div class="py-56 relative">
-      <div class="col-6">
-
+<template>
+  <div class="py-56 relative">
+    <div class="px-5 lg:px-40 container mx-auto flex flex-col-reverse lg:block">
+      <navbar class="z-30" />
+      <div class="lg:w-6/12">
+        <div class="text-left space-y-6">
+            <h1 class="text-5xl text-blue-900 font-medium font-sans">Introduce Yuor Product Quickly & Effectively</h1>
+            <div class="text-lg text-blue-600 space-y-6">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci assumenda veritatis fuga, illo a blanditiis quod sunt! Animi numquam, 
+              adipisci quasi obcaecati impedit, dolorem vitae amet non, tempore libero voluptatibus!</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Necessitatibus rem rerum perspiciatis accusantium corrupti optio minima asperiores</p>
+            </div>
+        </div>
+      
       </div>
-      <img class="col-8 absolute top-0 right-0 bottom-0 h-full object-cover" src="" />
-    </div> -->
-
-<template class="py-56 relative">
-  <navbar />
-  <div class="w-6/12"></div>
-  <div>
-  <div class="w-8/12">
-    <g-image src="~/assets/img-background.png" width="" />
-  </div>
+      <div class="lg:w-8/12">
+        <g-image class="lg:absolute top-0 right-0 bottom-0 object-cover" src="~/assets/img-background.png" width="" />
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script>
 import navbar from "~/components/navbar.vue";
